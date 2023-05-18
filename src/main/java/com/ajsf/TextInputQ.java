4dd90f54363f2793e33e6d142af915c013c73f69
Arun Fletcher <focalpear00@yahoo.co.uk>
2023-05-18 15:31:49 +0100

    Finished work on questions. Testing also complete.

--- src/main/java/com/ajsf/TextInputQ.java
+++ src/main/java/com/ajsf/TextInputQ.java
@@ -1,13 +1,10 @@
 package com.ajsf;
 
-import static com.ajsf.Main.inArray;
-
-import java.util.Scanner;
-
 // Text Input Question
 public class TextInputQ extends Question {
     // Attributes
     private final String[] correctEntries;
+
 
     // Constructor
     public TextInputQ(String source, String title, String correctEntries) {
@@ -15,29 +12,22 @@ public class TextInputQ extends Question {
         this.correctEntries = toArray(correctEntries);
     }
 
+
     // Question Output (toString)
     @Override
     public String toString() {
         return String.format("[From %s]\n%s", this.getSource(), this.getTitle());
     }
 
+    // Check Answer
+    @Override
+    public Boolean isCorrect(String entry) {
+        return inArray(entry, this.getCorrectEntries());
+    }
+
+
     // Accessors
     public String[] getCorrectEntries() {
         return correctEntries;
-    }
-
-    // Testing
-    public static void main(String[] args) {
-        TextInputQ q1 = new TextInputQ("Test Source", "This is a ____ question.", "[test;fake]");
-        Scanner in = new Scanner(System.in);
-
-        System.out.println(q1);
-        String entry = in.nextLine();
-
-        if (inArray(entry, q1.getCorrectEntries())) {
-            System.out.println("Correct");
-        } else {
-            System.out.println("Incorrect");
-        }
     }
 }
--- src/main/java/com/ajsf/TrueFalseQ.java
+++ src/main/java/com/ajsf/TrueFalseQ.java
@@ -2,18 +2,40 @@ package com.ajsf;
 
 public class TrueFalseQ extends Question {
     // Attributes
+    private final boolean answer;
+
 
     // Constructor
-    public TrueFalseQ(String source, String title) {
+    public TrueFalseQ(String source, String title, boolean answer) {
         super(source, title);
+        this.answer = answer;
     }
 
+
+    // Question Output (toString)
     @Override
     public String toString() {
-        return null;
+        return String.format("[From %s]\n%s", this.getSource(), this.getTitle());
     }
 
-    // Mutators
+
+    // Check Answer
+    public Boolean isCorrect(String entry) {
+        boolean userAnswer;
+        if (entry.equalsIgnoreCase("true")) {
+            userAnswer = true;
+        } else if (entry.equalsIgnoreCase("false")) {
+            userAnswer = false;
+        } else {
+            System.out.println("Answer should be either true or false.");
+            return false;
+        }
+        return userAnswer == this.getAnswer();
+    }
+
 
     // Accessors
+    public Boolean getAnswer() {
+        return answer;
+    }
 }
